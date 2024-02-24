bb9159486684b27dfce07adc8cfdf71b4e080a20
Sideloader9820 <160022449+Sideloader9820@users.noreply.github.com>
2024-02-24 14:55:24 +0000

    rising_d1: adapt to rising

--- rising_d1.mk
+++ rising_d1.mk
@@ -37,13 +37,13 @@ TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
 
 ## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rising/config/rising.mk)
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_d1
+PRODUCT_NAME := rising_d1
 PRODUCT_DEVICE := d1
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N970F
